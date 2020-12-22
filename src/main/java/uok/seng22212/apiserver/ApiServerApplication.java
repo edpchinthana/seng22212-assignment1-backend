fd07d201b4607a2523035cbe576cc56499f684ad
edpchinthana <edpchinthana@outlook.com>
2020-12-22 16:27:32 +0530

    initial commit

---
+++ src/main/java/uok/seng22212/apiserver/ApiServerApplication.java
@@ -0,0 +1,13 @@
+package uok.seng22212.apiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uok/seng22212/apiserver/ApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uok.seng22212.apiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
