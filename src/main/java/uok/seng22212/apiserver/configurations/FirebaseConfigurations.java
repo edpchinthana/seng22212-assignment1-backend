101c19f16f1440cae4168ace7166e219f31704ab
edpchinthana <edpchinthana@outlook.com>
2020-12-24 16:46:13 +0530

    firebase configurations

---
+++ src/main/java/uok/seng22212/apiserver/configurations/FirebaseConfigurations.java
@@ -0,0 +1,32 @@
+package uok.seng22212.apiserver.configurations;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.stereotype.Service;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Service
+public class FirebaseConfigurations {
+
+    public static void initializeFirebase() throws IOException {
+
+        FileInputStream serviceAccount =
+                new FileInputStream("/home/pasindu/Downloads/serviceAccountKey.json");
+                FirebaseOptions options = new FirebaseOptions.Builder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .build();
+                FirebaseApp.initializeApp(options);
+    }
+
+    public static Firestore getFirestoreReference(){
+        return FirestoreClient.getFirestore();
+    }
+
+
+
+}
---
+++ src/main/java/uok/seng22212/apiserver/repositories/firebase/SensorDataRepositoryImpl.java
@@ -0,0 +1,12 @@
+package uok.seng22212.apiserver.repositories.firebase;
+
+import uok.seng22212.apiserver.models.SensorData;
+import uok.seng22212.apiserver.models.SensorType;
+import uok.seng22212.apiserver.repositories.SensorDataRepository;
+
+public class SensorDataRepositoryImpl implements SensorDataRepository {
+    @Override
+    public void addSensorData(SensorType sensorType, SensorData sensorData) {
+
+    }
+}
