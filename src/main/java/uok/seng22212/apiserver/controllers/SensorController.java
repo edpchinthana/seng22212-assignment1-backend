815da599a28f5e2998dbd25dedf6f0fc87cf6c4d
Maleesha <maleeshamihiranga97@gmail.com>
2020-12-24 20:59:12 +0700

    Creating SensorController

---
+++ src/main/java/uok/seng22212/apiserver/controllers/SensorController.java
@@ -0,0 +1,16 @@
+package uok.seng22212.apiserver.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uok.seng22212.apiserver.models.SensorData;
+import  uok.seng22212.apiserver.services.SensorDataService;
+
+public class SensorController {
+
+
+
+}
---
+++ src/main/java/uok/seng22212/apiserver/services/SensorService.java
@@ -0,0 +1,13 @@
+package uok.seng22212.apiserver.services;
+
+import uok.seng22212.apiserver.models.SensorData;
+import uok.seng22212.apiserver.models.SensorType;
+
+import java.util.List;
+
+
+
+public interface SensorService {
+
+
+}
