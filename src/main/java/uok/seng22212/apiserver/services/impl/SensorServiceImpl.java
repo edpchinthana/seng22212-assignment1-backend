af961fb3ff10d50ce81b342e0767142e654b29cf
Maleesha <maleeshamihiranga97@gmail.com>
2020-12-24 21:46:55 +0700

    Updating

--- src/main/java/uok/seng22212/apiserver/services/impl/SensorServiceImpl.java
+++ src/main/java/uok/seng22212/apiserver/services/impl/SensorServiceImpl.java
@@ -5,10 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import uok.seng22212.apiserver.controllers.SensorDataController;
+import uok.seng22212.apiserver.controllers.SensorController;
 import uok.seng22212.apiserver.models.Sensor;
 import uok.seng22212.apiserver.models.SensorType;
-import uok.seng22212.apiserver.repositories.SensorDataRepository;
 import uok.seng22212.apiserver.repositories.SensorRepository;
 
 import uok.seng22212.apiserver.services.SensorService;
