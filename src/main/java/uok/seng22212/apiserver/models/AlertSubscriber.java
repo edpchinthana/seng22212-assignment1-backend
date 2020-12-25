0cf39f0e3d9a5a94fdf8b6f4eea6e88951a551d6
edpchinthana <edpchinthana@outlook.com>
2020-12-25 21:59:23 +0530

    AlertSubscriber model

---
+++ src/main/java/uok/seng22212/apiserver/models/AlertSubscriber.java
@@ -0,0 +1,48 @@
+package uok.seng22212.apiserver.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AlertSubscriber {
+
+    private String id;
+    private String displayName;
+    private String email;
+    private String phoneNo;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+}
