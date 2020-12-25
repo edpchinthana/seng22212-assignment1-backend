757378ca51d841ad13e11a69699f0995f20113e3
edpchinthana <edpchinthana@outlook.com>
2020-12-25 22:23:54 +0530

    alert subscriber repository

---
+++ src/main/java/uok/seng22212/apiserver/repositories/AlertSubscriberRepository.java
@@ -0,0 +1,12 @@
+package uok.seng22212.apiserver.repositories;
+
+import uok.seng22212.apiserver.models.AlertSubscriber;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public interface AlertSubscriberRepository {
+
+    void addSubscriber(AlertSubscriber alertSubscriber);
+    List<AlertSubscriber> getAllSubscribers() throws ExecutionException, InterruptedException;
+}
---
+++ src/main/java/uok/seng22212/apiserver/repositories/firebase/AlertSubscriberRepositoryImpl.java
@@ -0,0 +1,33 @@
+package uok.seng22212.apiserver.repositories.firebase;
+
+
+import com.google.cloud.firestore.Firestore;
+import uok.seng22212.apiserver.configurations.FirebaseConfigurations;
+import uok.seng22212.apiserver.models.AlertSubscriber;
+import uok.seng22212.apiserver.repositories.AlertSubscriberRepository;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class AlertSubscriberRepositoryImpl implements AlertSubscriberRepository {
+
+    private final static Firestore firestoreRef = FirebaseConfigurations.getFirestoreReference();
+
+    @Override
+    public void addSubscriber(AlertSubscriber alertSubscriber) {
+        try{
+            firestoreRef.collection("AlertSubscribers").document(alertSubscriber.getId()).set(alertSubscriber);
+        }catch (Exception e){
+            throw e;
+        }
+    }
+
+    @Override
+    public List<AlertSubscriber> getAllSubscribers() throws ExecutionException, InterruptedException {
+        try{
+            return firestoreRef.collection("AlertSubscribers").get().get().toObjects(AlertSubscriber.class);
+        }catch (Exception e){
+            throw e;
+        }
+    }
+}
